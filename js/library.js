d58590e8b407d28e3953ccd1e9163820c6b1aeef
Xavier Marques <xavierpmarques@gmail.com>
2021-05-28 23:36:54 +0100

    Add Book Class. Write functions that push book to Array and populate screen

--- js/library.js
+++ js/library.js
@@ -1,62 +1,109 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-const listOfBooksDiv = document.querySelector("#listOfBooksDiv");
-const bookTable = document.querySelector("#bookTable");
-
-// Book constructor
-function newBook(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-}
+const generalContainer = document.querySelector("#generalContainer");
+const libraryContainer = document.querySelector("#libraryContainer");
+const addBookButton = document.querySelector("#addBookButton");
+const titleInput = document.querySelector("#titleInput");
+const authorInput = document.querySelector("#authorInput");
+const pagesInput = document.querySelector("#pagesInput");
 
 let library = [];
 
-function addBookToLibrary (book) {
-    library.push(book);   
-}
-
-let hobbit = new newBook("Hobbit", "Tolkien", "230", "To Read");
-let pokemon = new newBook("Pokemon", "Japan", "125", "Read");
-addBookToLibrary(hobbit);
-addBookToLibrary(pokemon);
-
-
-function listOfBooks() {
-    for (let book of library){
-        let title = book.title;
-        let author = book.author;
-        let pages = book. pages;
-        let readStatus = book.readStatus
-    
-    // Create table row
-    let createRow = document.createElement("tr");
-    createRow.id = title;
-
-    // Create columns
-    let titleCell = document.createElement("td");
-    let authorCell = document.createElement("td");
-    let pagesCell = document.createElement("td");
-    let readStatusCell = document.createElement("td");
-    
-    // Give columns values
-    titleCell.innerText = title;
-    authorCell.innerText = author;
-    pagesCell.innerText = pages;
-    readStatusCell.innerText = readStatus;
-
-    // Append columns to the row
-    createRow.appendChild(titleCell);
-    createRow.appendChild(authorCell);
-    createRow.appendChild(pagesCell);
-    createRow.appendChild(readStatusCell);
-    
-    // Append row to table
-    console.log(createRow);
-    bookTable.appendChild(createRow);
+// Book Class
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = "Not Finished"
     }
-}
-listOfBooks();
 
-});
+
+    populateLibraryContainerWithBooks() {
+        for (let book of library){
+            let title = book.title;
+            let author = book.author;
+            let pages = book.pages;
+            let readStatus = book.readStatus;
+
+        // have to right a function here that checks if book is already in library and returs blank
+        
+        // Create Div
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("bookExample");
+    
+        // Create p elements
+        let deleteBookP = document.createElement("p")
+        let titleP = document.createElement("p");
+        let authorP = document.createElement("p");
+        let pagesP = document.createElement("p");
+        let readStatusP = document.createElement("p")
+
+        deleteBookP.classList.add("deleteBook")
+        titleP.classList.add("title");
+        authorP.classList.add("author");
+        pagesP.classList.add("pages");
+        readStatusP.classList.add("readStatus");
+        
+        // Give <p> elements values
+        deleteBookP.innerHTML = "\&#9746"
+        titleP.innerText = title;
+        authorP.innerText = author;
+        pagesP.innerText = pages;
+        readStatusP.innerText = readStatus;
+    
+        // Append <p> elemnts to the Div
+        newDiv.appendChild(deleteBookP);
+        newDiv.appendChild(titleP);
+        newDiv.appendChild(authorP);
+        newDiv.appendChild(pagesP);
+        newDiv.appendChild(readStatusP);
+        
+        // Append div to General Container
+        libraryContainer.appendChild(newDiv);
+        }
+
+    }
+
+
+    addBookToLibrary () {
+        library.push(this);   
+    }
+
+    sameTitle(thisBookTitle, library) {
+        for (let i = 0; i < library.length; i++) {
+            if (library[i].title === thisBookTitle) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+    
+}
+
+// Update Array and Show Books Ater Clikcing "Add Book"
+addBookButton.addEventListener("click", pushBookToArray);
+
+function pushBookToArray() {
+        if (!titleInput.value || !authorInput.value || !pagesInput.value ) {
+            return
+        }
+        let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
+        
+        // if book by same title already exists don't add to library
+        if(!newBook.sameTitle(newBook.title, library)) {
+            newBook.addBookToLibrary();
+        }
+
+        newBook.populateLibraryContainerWithBooks();  
+        titleInput.value = "";
+        authorInput.value = "";
+        pagesInput.value = "";
+}
+
+
+})
+
+
+
